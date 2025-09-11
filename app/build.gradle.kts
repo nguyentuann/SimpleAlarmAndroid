080fd71236c53ad9990c70c5ca377523f8de169b
Nhat Tuan <nhattuan242@gmail.com>
2025-09-11 16:49:44 +0700

    feat: Implement basic top app bar
    This commit introduces a basic top app bar with:
    - Title "My Alarms"
    - Four action buttons: Timer, Quick Alarm, Setting, and Add
    - New icons for the action buttons
    - Basic styling for the app bar and action buttons, including background colors, text colors, and font styles.
    - New string resources for the action buttons' content descriptions.
    - New color resources for UI elements.
    - New font resources (Open Sans) for text styling.
    - Enabled ViewBinding and DataBinding.
    - Updated dependencies.
    - Changed MainActivity to extend AppCompatActivity and set the layout.
    - Updated theme to support a transparent status bar and light status bar icons.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "vn.tutorial.simplealarmandroid"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "vn.tutorial.simplealarmandroid"
@@ -36,6 +36,8 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
+        dataBinding = true
     }
 }
 
@@ -49,6 +51,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
